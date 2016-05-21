7a4341f6217c00f60c29eca627d22b6eff28a843
hyperbolee <slalli4251@gmail.com>
2016-05-21 18:57:11 -0400

    included ROOT headers so I can compile if I want. Also made small tweaks

--- analysistools.C
+++ analysistools.C
@@ -1,8 +1,32 @@
 //  analysistools.C
+//
 //  Functions that are used in multiple
 //  places in my analysis code to reduce redundancy
 //
 //  Author: Lee Allison, Apr.29.2016
+
+// include ROOT headers
+#include "TCanvas.h"
+#include "TCollection.h"
+#include "TError.h"
+#include "TF1.h"
+#include "TFile.h"
+#include "TGraph.h"
+#include "TH1D.h"
+#include "TLegend.h"
+#include "TList.h"
+#include "TROOT.h"
+#include "TSpectrum.h"
+#include "TString.h"
+#include "TStyle.h"
+#include "TSystem.h"
+#include "TSystemDirectory.h"
+#include "TSystemFile.h"
+#include "TTree.h"
+
+// don't include more than once
+#ifndef analysistools
+#define analysistools
 
 /*
   Set default style for all macros
@@ -21,7 +45,8 @@ void SetStyle()
 */
 void StyleGraph(TGraph *&gr, double min, double max,
 				TString xtitle = "", TString ytitle = "",
-				int marker = 20, Color_t color = kBlue)
+				int marker = 20, Color_t color = kBlue,
+				TString name = "", TString title = "")
 {
 	// axis styling
 	gr->GetYaxis()->SetRangeUser(min,max);
@@ -31,6 +56,10 @@ void StyleGraph(TGraph *&gr, double min, double max,
 	// marker styling
 	gr->SetMarkerStyle(marker);
 	gr->SetMarkerColor(color);
+
+	// name and title
+	gr->SetName(name);
+	gr->SetTitle(title);
 }
 
 /*
@@ -54,7 +83,8 @@ double NormSim(TH1D *&sim, TH1D *&dat)
 */
 void BGSub(TH1D *&sim, TH1D *&dat, TTree *&tree, 
 		   int bins, TString pidcut = "PID>1000",
-		   Color_t fill = kBlue, int style = 3002)
+		   Color_t fill = kBlue, int style = 3002,
+		   int smooth = 0)
 {   // normalize simulation
 	double scale = NormSim(sim,dat);
 
@@ -62,6 +92,8 @@ void BGSub(TH1D *&sim, TH1D *&dat, TTree *&tree,
 	TH1D *bg = new TH1D("bg","bg",bins,0.6,1);
 	tree->Project("bg","theta",Form("%s && path!=LUTpath",pidcut.Data()));
 	bg->Scale(scale);
+
+	if(smooth>0) bg->Smooth(smooth);
 
 	// subtract background and style
 	sim->Add(bg,-1);
@@ -103,7 +135,7 @@ double* SpecSearch(TSpectrum *&spec, TH1D *&hist, TF1 *&fit)
     // uses a TSpectrum to find a peak in hist
 	// and set mean and multiplicative constant
 	double con(0), mean(0), sigma(0);
-	int nfound = spec->Search(hist,2,"nodraw",0.9);
+	int nfound = spec->Search(hist,2,"nodraw",0.6);
 	if(nfound)
 	{ // if peak found use spectrum peak
 		mean = spec->GetPositionX()[0];
@@ -111,9 +143,11 @@ double* SpecSearch(TSpectrum *&spec, TH1D *&hist, TF1 *&fit)
 	}
 	else
 	{ // if no peak found use usual method
+	  // 
 		mean = hist->GetBinCenter(hist->GetMaximumBin());
 		if(mean>0.86) mean = 0.82; // stay within range 
 		con  = hist->GetMaximum();
+		cout << "didn't find peak" << endl;
 	}
 
 	// fit hist
@@ -126,8 +160,43 @@ double* SpecSearch(TSpectrum *&spec, TH1D *&hist, TF1 *&fit)
 	con = fit->GetParameter(0);
 	mean = fit->GetParameter(1);
 	sigma = fit->GetParameter(2);
-	double parms[] = {con, mean, sigma};
-
+	double *parms = new double[3];
+	parms[0] = con;
+	parms[1] = mean;
+	parms[2] = sigma;
 	return parms;
 }
 
+/*
+TCanvas* drawFish(TTree *hits, TString selection = "PID>1000",
+				  TString digidata = "", int layout = 3,
+				  double maxz = -2, double minz = -2)
+{   // assign mcpid and pixid from the hits tree
+	int mcpid(0), pixid(0);
+	hits->SetBranchAddress("mcp",&mcpid);
+	hits->SetBranchAddress("pix",&pixid);
+    
+	// entry by entry selection
+	TTreeFormula* tform 
+		= new TTreeFormula("tree selection",selection,hits);
+
+	// initialize pad and color scale from prttools
+	initDigi();
+	SetRootPalette(1);
+	int entries = hits->GetEntries();
+	for (int i_entry=0; i_entry<entries; i_entry++)
+	{
+		hits->GetEntry(i_entry);
+		if(tform->EvalInstance() == 0) continue;
+	    
+	    fhDigi[mcpid]->Fill(pixid%8, pixid/8);
+	}
+
+	drawDigi("m,p,v\n",3,-2,-2);
+	cDigi->cd();
+	(new TPaletteAxis(0.90,0.1,0.94,0.90,fhDigi[0]))->Draw();
+
+	return cDigi;
+	}*/
+
+#endif
